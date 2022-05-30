4a0744f8e3543e75e4c3d3cd1c21c47a1d9a6c47
luu tuan dung <luutuandung102@gmail.com>
2022-05-30 20:15:47 +0700

    fix call api

--- src/App.js
+++ src/App.js
@@ -1,40 +1,48 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import axios from "axios";
 import WeatherInfo from "./Components/WeatherInfo";
 
 function App() {
-  const [searchQuery, setSearchQuery] = useState("");
-  const [timeoutId, setTimeoutId] = useState();
   const [weatherList, setWeatherList] = useState();
+  const [loading, setLoading] = useState(false);
 
   const fetchData = async (searchString) => {
+    setLoading(true);
     const response = await axios.get(
       `http://api.weatherapi.com/v1/forecast.json?key=62510fd6820a495880f21357223005&q=${searchString}&days=5&aqi=no&alerts=no`
     );
+    setLoading(false);
     setWeatherList(response.data);
   };
 
-  const onTextChange = (event) => {
-    clearTimeout(timeoutId);
-    setSearchQuery(event.target.value);
-    const timeout = setTimeout(() => fetchData(event.target.value), 500);
-    setTimeoutId(timeout);
+  const onPressEnter = (event) => {
+    if (event.key === "Enter") {
+      search();
+    }
   };
+
+  const search = () => {
+    const value = searchInput.current.value;
+    fetchData(value);
+  };
+
+  const searchInput = useRef("");
 
   return (
     <div className="app">
       <div id="searchBox">
-        <img id="searchIcon" src="/search-icon.svg" alt="" />
+        <img onClick={search} id="searchIcon" src="/search-icon.svg" alt="" />
         <input
+          ref={searchInput}
           type="text"
           id="searchInput"
           placeholder="Search"
-          value={searchQuery}
-          onChange={onTextChange}
+          onKeyPress={onPressEnter}
         />
       </div>
       <div className="container">
-        <WeatherInfo item={weatherList ? weatherList : {}} />
+        {!loading && weatherList && <WeatherInfo item={weatherList} />}
+        {loading && "Loading..."}
       </div>
     </div>
   );
--- src/Components/WeatherInfo.js
+++ src/Components/WeatherInfo.js
@@ -1,11 +1,24 @@
+const getDay = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const checkDay = {};
+
 const WeatherInfo = (props) => {
-  console.log(props.item);
   const { forecast } = props?.item;
   return (
     <div className="weatherList">
       {forecast?.forecastday?.map((item, index) => (
         <div className="weatherItem" key={index}>
-          <div className="weatherDay">{item.date}</div>
+          <div className="weatherDay">
+            {getDay[new Date(item.date).getDay()]}
+          </div>
           <div className="weatherMin">Min: {item.day.maxtemp_c}</div>
           <div className="weatherMax">Max: {item.day.maxtemp_c}</div>
         </div>
